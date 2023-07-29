1aa76d0e99fd1e0e1ffb84443e749fce9eba0f6c
redocbew <redocbew@bitbasher.home>
2023-07-29 09:16:06 -0700

    Sending backups to a network share

--- scripts/bash/vmbackup.sh
+++ scripts/bash/vmbackup.sh
@@ -4,7 +4,15 @@
 Collect VM disk images and configuration and copy them elsewhere for easier backup.
 '
 
-# 1. Copy vm configuration and images to a single location on local storage for easier backup
+if ! [[ $(findmnt -M "/media/backups") ]]; then
+    mount -t nfs stockpile.home:/mnt/stockpile/backups /media/backups
+fi
+
+if ! [[ $(findmnt -M "/media/backups") ]]; then
+    exit 1
+fi
+
+# Copy vm configuration and images to a single location on local storage for easier backup
 for vm in $(virsh list --name --all)
 do
   vmstate=$(virsh domstate $vm);
@@ -18,16 +26,16 @@ do
   vmdisk=$(virsh domblklist $vm --details | awk '/disk/{print $4}');
   israw=$(echo $vmdisk | grep -c ^/dev/);
 
-  mkdir -p /tank/vmbackup/$vm;
-  virsh dumpxml $vm > /tank/vmbackup/$vm/$vm.xml;
+  virsh dumpxml $vm > /media/backups/$vm/$vm.xml;
 
   if [ $israw = 1 ]; then
-    qemu-img convert -f raw -O qcow2 $vmdisk /tank/vmbackup/$vm/$vm.qcow2;
+    qemu-img convert -f raw -O qcow2 $vmdisk /media/backups/$vm/$vm.qcow2;
   else
-    cp $vmdisk /tank/vmbackup/$vm/$vm.qcow2;
+    cp $vmdisk /media/backups/$vm/$vm.qcow2;
   fi
 
   if [ "$startvm" = true ]; then
     virsh start $vm;
   fi
 done
+
